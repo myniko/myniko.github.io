cb87ff48c4b9a820025b93016d7ec9db65081a97
myniko <d.madro97@gmail.com>
2017-08-18 11:07:03 +0200

    Add files via upload

---
+++ Agency/custom.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $(".owl-carousel").owlCarousel({
+    autoplay:true,
+        loop:true,
+        margin:0,
+        responsive:{
+            0:{items:2},
+            600:{items:2},
+            1000:{items:2},
+        }
+})              
+})
